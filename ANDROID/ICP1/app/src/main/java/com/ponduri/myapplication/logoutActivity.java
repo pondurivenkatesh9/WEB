b4856ead0a7eade81d69c165c8ba7d3b28274472
Your Name <pondurivenkatesh9@gmail.com>
2018-11-11 21:18:48 -0600

    UPDATE

--- ANDROID/ICP1/app/src/main/java/com/ponduri/myapplication/logoutActivity.java
+++ ANDROID/ICP1/app/src/main/java/com/ponduri/myapplication/logoutActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
 
 public class logoutActivity extends AppCompatActivity {
 
@@ -12,6 +16,20 @@ public class logoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
     }
+
+    public void Save(String fileName) {
+        EditText1 = (EditText)findViewById(R.id.EditText1)
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(EditText1.);
+            out.close();
+            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
     public void logout(View view) {
         Intent redirect = new Intent(logoutActivity.this, MainActivity.class);
         startActivity(redirect);
